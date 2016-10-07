979cce4d0e6c08e491839416061291ef229c831f
Ju <findshawn@163.com>
2016-10-06 22:28:40 -0500

    course 9 week 3 update

---
+++ 9 - Developing Data Products/week 3/topten/R/hello.R
@@ -0,0 +1,17 @@
+#' do a test
+#'
+#' lalalalalala
+#'
+#' @author Shang Ju
+#' @param x int
+#' @return a vector of random numbers
+#' @details
+#' lalalalalalalallalalalala
+#' @export
+#' @importFrom stats rnorm
+
+test <- function(x) {
+  rnorm(x)
+}
+
+
