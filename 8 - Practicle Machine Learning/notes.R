db9a8a038eef59c96d9af8b95d1d741557d79118
Shang Ju <findshawn@163.com>
2016-10-16 22:52:11 -0500

    init course 8

---
+++ 8 - Practicle Machine Learning/notes.R
@@ -0,0 +1,16 @@
+# week 1
+
+library(caret)
+library(kernlab)
+data(spam)
+inTrain <- createDataPartition(y=spam$type,p=0.75,list=FALSE)
+training <- spam[inTrain,]
+testing <- spam[-inTrain,]
+dim(training)
+set.seed(32343)
+fit <- train(type ~., data=training, method='glm')
+fit
+fit$finalModel
+predictions <- predict(fit,newdata=testing)
+predictions
+confusionMatrix(predictions,testing$type)
